d1458276894fcd4aa691790ce9b1b5e2d43a2a3b
Andreuparri <andreuparri@gmail.com>
2015-12-19 10:57:26 +0000

    basic setup working
    nice constant margin working

--- learning_d3_v001/js/main.js
+++ learning_d3_v001/js/main.js
@@ -12,28 +12,18 @@ var x = d3.scale.linear()
     .domain([0,d3.max(data)])
     .range([0,100]);
 
-    var x2 = d3.scale.linear()
-    .domain([0,data.length])
-    .range([0,100]);
-
-var y = ($(window).height()-120)/(data.length-1);
+var y = 100/(data.length);
 	//y = y-1;
   console.log(y);
 
-d3.select(".chart")
+d3.select(".container")
   .selectAll("div")
     .data(data)
   .enter().append("div")
-  	.style("width",function(d) { return x(d) + "%"; })
-  	.style("height", "10%")
-    .style("position","absolute")
-    .style("top",function(d,i) { return x2(i) + "%"; })
-  	.style("background-color","orange")
-    .style("border","1px solid black")
-
-    ;
+    .attr("class","bars")
+    .style("height", y + "%")
+    .style("width",function(d) { return x(d) + "%"; });
   	//.text(function(d) { return d; });
 
 
 });
-
