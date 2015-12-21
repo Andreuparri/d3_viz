8082d4fd3898428d019cdf0edc59f8a88026b406
Andreuparri <andreuparri@gmail.com>
2015-12-21 11:04:43 +0000

    sorted and with data changing

--- learning_d3_v002/js/main.js
+++ learning_d3_v002/js/main.js
@@ -4,32 +4,44 @@ $( document ).ready(function() {
 //create the data
 var data = [];
 
-function changeData(data){
-    for (var i = 0; i <35; i++) {
+function changeData(){
+    data = [];  
+    for (var i = 0; i <25; i++) {
   	   data.push(Math.random());
-      };
-      return data;
+      }
+      data.sort();
+      //console.log(data);
 }
 
+
 //change the data on intervals
-setInterval(changeData,2000);
+//setInterval(changeData,2000);
+function renderData(){
 
+    var x = d3.scale.linear()
+        .domain([0,d3.max(data)])
+        .range([0,100]);
 
-var x = d3.scale.linear()
-    .domain([0,d3.max(data)])
-    .range([0,100]);
+    var y = 100/(data.length);
+    	//y = y-1;
+      //console.log(y);
 
-var y = 100/(data.length);
-	//y = y-1;
-  console.log(y);
+    var bar = d3.select(".container")
+      .selectAll("div")
+        .data(data);
+      bar.enter().append("div")
+        .attr("class","bars");
+       bar
+        .style("height", y + "%")
+        .style("width",function(d) { return x(d) + "%"; });
 
-d3.select(".container")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .attr("class","bars")
-    .style("height", y + "%")
-    .style("width",function(d) { return x(d) + "%"; });
-	//.text(function(d) { return d; });
+    //console.log("Render");
+    	//.text(function(d) { return d; });
+
+}
+changeData();
+renderData();
+setInterval(changeData,50);
+setInterval(renderData,5000);
 
 });
